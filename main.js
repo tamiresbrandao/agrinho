2b40d3f622730675dfe4bab8b5012c7423aef156
tamiresbrandao <167766003+tamiresbrandao@users.noreply.github.com>
2026-06-08 12:15:58 -0300

    s

--- main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("btn");
-const mensagem = document.getElementById("mensagem");
-
-btn.addEventListener("click", () => {
-    mensagem.innerHTML =
-    "🌱 A união entre campo e cidade fortalece a economia, promove sustentabilidade e melhora a qualidade de vida de todos.";
-});
